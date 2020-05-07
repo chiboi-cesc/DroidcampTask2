19406f7d60d088668178242f54c45d3fb5d52105
chiboi-cesc <chiboicesc@gmail.com>
2020-05-07 22:03:33 +0000

    first commit

---
+++ src/droidCampMidWeekTask.kt
@@ -0,0 +1,11 @@
+fun main (){
+    calculation()
+}
+
+fun calculation(){
+    var a = 4
+    var b = 3
+    println("The sum of(a and b): a + b = ${a + b}")
+    println("The Difference of(a and b): a - b = ${a - b}")
+    println("The Product of(a and b): a * b = ${a * b}")
+}
